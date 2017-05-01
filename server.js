d0f841d5ce8a5210070fe30df3dfd3feabcbc8b9
Terry Chong <tc216997@gmail.com>
2017-05-02 05:53:05 +0800

    fixed contact page

--- server.js
+++ server.js
@@ -1,33 +1,49 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const server = express();
 
-server.set('port', process.env.PORT || 3000 )
-
+server.set('port', process.env.PORT || 3000 );
+server.use(bodyParser.urlencoded({extended:true}));
 server.use(express.static(path.resolve(__dirname, 'public')));
 
 server.get('/', (req, res) => {
-  res.sendFile(getFile('index'))
+  res.sendFile(getFile('index'));
 });
 
 server.get('/science/', (req, res) => {
-  res.sendFile(getFile('science'))
+  res.sendFile(getFile('science'));
 });
 
 server.get('/testimonials/', (req, res) => {
-  res.sendFile(getFile('testimonials'))
+  res.sendFile(getFile('testimonials'));
 });
 
 server.get('/demo/', (req, res) => {
-  res.sendFile(getFile('demo'))
+  res.sendFile(getFile('demo'));
 });
 
 server.get('/shop/', (req, res) => {
-  res.sendFile(getFile('shop'))
+  res.sendFile(getFile('shop'));
 });
 
 server.get('/contact/', (req, res) => {
-  res.sendFile(getFile('contact'))
+  res.sendFile(getFile('contact'));
+});
+
+server.get('/email-sent', (req, res) => {
+  res.status(200).sendFile(getFile('email-sent'));
+});
+
+server.post('/send-email', (req, res) => {
+
+  console.log(req.body.email);
+  console.log(req.body.name);
+  console.log(req.body.subject);
+  console.log(req.body.message);
+  res.status(200).redirect('/email-sent');
+
 });
 
 // 404 and 500 status error handling
